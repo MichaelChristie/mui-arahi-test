7aaa321d74bfcb897d84c69db98d48dabe192017
Michael Christie <christie.michael@gmail.com>
2022-08-31 07:37:56 +1200

    pre grid messing about

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -4,25 +4,23 @@ import { Grid, Box } from "@mui/material/";
 export default function PageLayout({ children }) {
   return (
     <>
-      <Box>
-        <Grid
-          container
-          direction="column"
-          sx={{
-            position: "relative",
-            zIndex: 1,
-          }}
-        >
-          <Grid item></Grid>
-          <Grid item container>
-            <Grid item xs={false} xl={1} />
-            <Grid item xs={12} xl={10} className="showMe">
-              {children}
-            </Grid>
-            <Grid item xs={false} xl={1} />
+      <Grid
+        container
+        direction="column"
+        sx={{
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <Grid item></Grid>
+        <Grid item container>
+          <Grid item xs={false} xl={1} className="showMe" />
+          <Grid item xs={12} xl={10}>
+            {children}
           </Grid>
+          <Grid item xs={false} xl={1} className="showMe" />
         </Grid>
-      </Box>
+      </Grid>
     </>
   );
 }
--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,171 +1,204 @@
 import * as React from "react";
 import { Grid, Box, Paper, Typography, Divider } from "@mui/material/";
 import ClaimMenu from "./navigation/ClaimMenu";
+import { styled } from "@mui/material/styles";
 import ProductGridDropdownMenu from "./navigation/ProductGridDropdownMenu";
 
 export default function ProductGrid() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
   return (
     <>
-      <Box
-        sx={{
-          px: {
-            xs: 3,
-            sm: 3,
-            md: 3,
-            lg: 3,
-            xl: 3,
-          },
-        }}
-      >
-        <Grid container direction="column">
-          <Grid item container>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-car"></div>
-              <ProductGridDropdownMenu>Car</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-home"></div>
-              <ProductGridDropdownMenu>Home</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-contents"></div>
-              <ProductGridDropdownMenu>Contents</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-landlord"></div>
-              <ProductGridDropdownMenu>Landlord</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-small-business"></div>
-              <ProductGridDropdownMenu>Small Business</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-motorcycle"></div>
-              <ProductGridDropdownMenu>Motorcycle</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-caravan"></div>
-              <ProductGridDropdownMenu>Caravan</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-driver"></div>
-              <ProductGridDropdownMenu>Classic Car</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-life"></div>
-              <ProductGridDropdownMenu>Life</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-health"></div>
-              <ProductGridDropdownMenu>Health</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-travel"></div>
-              <ProductGridDropdownMenu>Travel</ProductGridDropdownMenu>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              md={3}
-              lg={2}
-              align="center"
-              // sx={{ mx: 0 }}
-              className="showMe"
-            >
-              <div className="icon circle-sm size-72 aai-icon-pets"></div>
-              <ProductGridDropdownMenu>Pets</ProductGridDropdownMenu>
-            </Grid>
-          </Grid>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
         </Grid>
-      </Box>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+        <Grid item xs={6} md={3} lg={2}>
+          <Item>iconHere</Item>
+        </Grid>
+      </Grid>
+
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-car"></div>
+          <ProductGridDropdownMenu>Car</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-home"></div>
+          <ProductGridDropdownMenu>Home</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-contents"></div>
+          <ProductGridDropdownMenu>Contents</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-landlord"></div>
+          <ProductGridDropdownMenu>Landlord</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-small-business"></div>
+          <ProductGridDropdownMenu>Small Business</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-motorcycle"></div>
+          <ProductGridDropdownMenu>Motorcycle</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-caravan"></div>
+          <ProductGridDropdownMenu>Caravan</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-driver"></div>
+          <ProductGridDropdownMenu>Classic Car</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-life"></div>
+          <ProductGridDropdownMenu>Life</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-health"></div>
+          <ProductGridDropdownMenu>Health</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-travel"></div>
+          <ProductGridDropdownMenu>Travel</ProductGridDropdownMenu>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          lg={2}
+          align="center"
+          // sx={{ mx: 0 }}
+          className="showMe1"
+        >
+          <div className="icon circle-sm size-72 aai-icon-pets"></div>
+          <ProductGridDropdownMenu>Pets</ProductGridDropdownMenu>
+        </Grid>
+      </Grid>
     </>
   );
 }
